7339ff5d35589a08f121124ce3ede512ac86a449
Devashish <devashish.86@gmail.com>
2018-09-11 13:32:36 -0700

    fixing setup script with cli options

--- devbox/setup.sh
+++ devbox/setup.sh
@@ -4,19 +4,10 @@ set -o pipefail
 IFS=$'\n\t'
 
 readonly ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export OPTS="$1"
 
 # Using bash infinity: https://github.com/niieani/bash-oo-framework
-#source "$ROOT_DIR/lib/oo-bootstrap.sh"
-
-# bash infinity based imports
-#import util/log
-#import util/exception
-##############################
-## parse args to process in two modes: local vs devbox
-## local will install docker, get IP of the machine and use docker container to
-## ssh into the machine and run the commands
-## syntax
-## ./setup.sh <mode> [local|devbox]
+source "$ROOT_DIR/lib/slog.sh"
 
 ##TODO
 # generate a private-public key pair if not present already
@@ -24,10 +15,13 @@ readonly ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # exit out if no keys present
 
 __bootstrap_desktop() {
+  log_info "Setting up desktop"
+  local envs=""
+  envs="$envs -e ANSIBLE_STDOUT_CALLBACK=debug"
   local cmd="docker run  \
     --rm \
     -v $(pwd):/ansible \
-    -e ANSIBLE_STDOUT_CALLBACK=debug \
+    $envs \
     ric03uec/cansible:master \
     -vv \
     -i /ansible/hosts \
@@ -35,21 +29,54 @@ __bootstrap_desktop() {
   eval "$cmd"
 }
 
-__bootstrap_system() {
+__bootstrap_remote() {
+  log_info "Setting up remote machine(s)"
+
+  local envs=""
+  envs="$envs -e ANSIBLE_STDOUT_CALLBACK=debug"
+
   local cmd="docker run  \
     --rm \
     -v $(pwd):/ansible \
-    -e ANSIBLE_STDOUT_CALLBACK=debug \
+    $envs \
     ric03uec/cansible:master \
     -vv \
     -i /ansible/hosts \
     /ansible/bootstrap_system.yml"
+  log_info "Executing $cmd"
   eval "$cmd"
 }
 
+__print_help() {
+  echo "
+  Usage:
+    ./setup.sh <command>
+
+  Commmands:
+    remote          Run Shippable installation
+    desktop         Run silent upgrade without any prompts
+    help            Print this message
+  "
+  exit 0
+}
+
 main() {
-  echo "Configuring development machine(s)"
-  __bootstrap_system
+  log_info "Configuring development machine(s)"
+  if [ -z "$OPTS" ]; then
+    log_warning "No options specified for setup."
+    __print_help
+    exit 0
+  elif [ "$OPTS" == "remote" ]; then
+    __bootstrap_remote
+    exit 0
+  elif [ "$OPTS" == "desktop" ]; then
+    __bootstrap_desktop
+    exit 0
+  else
+    log_error "Invalid options: $OPTS"
+    __print_help
+    exit 1
+  fi
 }
 
 main
